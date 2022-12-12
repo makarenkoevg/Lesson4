51613cf244ba45831d3b9b8ca0c9d94ee7447f02
makarenkoevg <makarenkoevg@mail.ru>
2022-12-12 21:50:43 +0500

    25.

--- 25/Program.cs
+++ 25/Program.cs
@@ -1,1 +1,17 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int Powr(int num1, int num2)
+{
+    int count = 1;
+  
+    for (int i = 1; i<=num2; i++)
+    {
+        count*=num1;
+
+    }
+    return count;
+}
+Console.Write("Введите число A:");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число B:");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine($"A в степени B = {Powr(a,b)}");
